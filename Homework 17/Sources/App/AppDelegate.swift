784a6db426297c3e7ad8a173a8bef2a0683df0f6
korotkovak <kristinakorotkova059@gmail.com>
2023-02-13 15:19:24 +0800

    Preparing the file

---
+++ Homework 17/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Homework 17
+//
+//  Created by Kristina Korotkova on 13/02/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
